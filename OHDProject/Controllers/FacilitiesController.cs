0aca5fe2c3cc5d70a54e7d683c24fa1093645157
nguyenlinh3836 <54592412+nguyenlinh3836@users.noreply.github.com>
2021-02-24 11:23:00 +0700

    fix

---
+++ OHDProject/Controllers/FacilitiesController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using OHDProject.Models;
+
+namespace OHDProject.Controllers
+{
+    public class FacilitiesController : Controller
+    {
+        private readonly OHDDbContext _context;
+
+        public FacilitiesController(OHDDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Facilities
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Facilities.ToListAsync());
+        }
+
+        // GET: Facilities/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var facility = await _context.Facilities
+                .FirstOrDefaultAsync(m => m.FacilityId == id);
+            if (facility == null)
+            {
+                return NotFound();
+            }
+
+            return View(facility);
+        }
+
+        // GET: Facilities/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Facilities/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("FacilityId,FacilityName")] Facility facility)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(facility);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(facility);
+        }
+
+        // GET: Facilities/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var facility = await _context.Facilities.FindAsync(id);
+            if (facility == null)
+            {
+                return NotFound();
+            }
+            return View(facility);
+        }
+
+        // POST: Facilities/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("FacilityId,FacilityName")] Facility facility)
+        {
+            if (id != facility.FacilityId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(facility);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!FacilityExists(facility.FacilityId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(facility);
+        }
+
+        // GET: Facilities/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var facility = await _context.Facilities
+                .FirstOrDefaultAsync(m => m.FacilityId == id);
+            if (facility == null)
+            {
+                return NotFound();
+            }
+
+            return View(facility);
+        }
+
+        // POST: Facilities/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var facility = await _context.Facilities.FindAsync(id);
+            _context.Facilities.Remove(facility);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool FacilityExists(int id)
+        {
+            return _context.Facilities.Any(e => e.FacilityId == id);
+        }
+    }
+}
